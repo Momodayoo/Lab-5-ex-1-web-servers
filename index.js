b6be261d3c653a12c0a69a98172c5cc0a0a87e69
Momodayoo <msyauqi94@gmail.com>
2023-11-09 20:26:53 +0800

    multi on 3000 and 3001 made

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+const app1 = express()
+const port1 = 3001
 
-const testRoutes = require('./routes/myTestRoutes');
-const calculatorRoutes = require('./routes/myTestRoutes');
-const userRoutes = require ('./routes/userRoutes')
-const app = express();
-// const app1 = express();
-const port = 3000;
-// const port1 =3001;  
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-app.use(express.json());
+app.get('/test', (req, res) => {
+    res.send('This is a test')
+    })
 
-app.use ('/', express.static('public'));
+app1.get('/', (req, res) => {
+    res.send('yarr!')
+})
 
-app.use('/test', testRoutes);
-
-app.use('/calculator', calculatorRoutes);
-
-app.use('/users', userRoutes);
-
-// app.get('/', (req, res) => {
-//     res.send ("Hellow World!");
-// });
-
-// app1.get ("/", (req, res) => {
-//     res.send ("Hello from the other side...");
-// });
-
-// app.get('/mytest', (req,res) => {
-//     res.send ("This is a test");
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening
-at http://localhost:${port}`);
-});
+at http://localhost:${port}`)
+})
 
-// app1.listen(port1, () => {
-//     console.log(`app1 started`);
-// });
-
+app1.listen(port1, () => {
+    console.log(`Example app listening
+at http://localhost:${port}`)
+})
